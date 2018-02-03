64b47279fc8f511d27b4db406a7a1025d694cefe
iraborysenko <aurorasray@gmail.com>
2018-02-03 22:54:33 +0200

    adding row dynamically

--- app/src/main/java/com/example/aurora/moviesineedtowatch/ui/SearchActivity.java
+++ app/src/main/java/com/example/aurora/moviesineedtowatch/ui/SearchActivity.java
@@ -7,6 +7,10 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.example.aurora.moviesineedtowatch.R;
@@ -26,6 +30,8 @@ import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by Android Studio.
@@ -39,108 +45,174 @@ public class SearchActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_search);
 
-        ConnectivityManager connMgr = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
-        assert connMgr != null;
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        if (networkInfo != null && networkInfo.isConnected()) {
-            new TMDBSearchManager().execute();
-        } else {
-            TextView textView = new TextView(this);
-            textView.setText("No network connection.");
-            setContentView(textView);
-            Log.e(Const.ERR, "stepErr");
+        TableLayout tableLayout = (TableLayout) findViewById(R.id.tlMarksTable);
+
+        List<String> competitoinsIDs = new LinkedList<String>();
+        List<String> marks = new LinkedList<String>();
+        List<String> numOfQuestions = new LinkedList<String>();
+
+
+// adding static values to test the layout. use your dynamic data here
+        competitoinsIDs.add("123");
+        competitoinsIDs.add("124");
+        competitoinsIDs.add("125");
+
+        marks.add("56");
+        marks.add("57");
+        marks.add("58");
+
+        numOfQuestions.add("10");
+        numOfQuestions.add("11");
+        numOfQuestions.add("12");
+
+        TableRow tableRows[] = new TableRow[competitoinsIDs.size()];
+        for (int i = 0; i < tableRows.length; i++) {
+
+            tableRows[i] = new TableRow(this);
+            tableRows[i].setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            tableRows[i].setWeightSum(2.0f);
+            tableRows[i].setPadding(5, 5, 5, 5);
+
+            TextView text = new TextView(this);
+            text.setLayoutParams(new android.widget.TableRow.LayoutParams(android.widget.TableRow.LayoutParams.WRAP_CONTENT,
+                    android.widget.TableRow.LayoutParams.WRAP_CONTENT, 1.0f));
+            text.setText(competitoinsIDs.get(i));
+            tableRows[i].addView(text);
+
+            text = new TextView(this);
+            text.setLayoutParams(new android.widget.TableRow.LayoutParams(android.widget.TableRow.LayoutParams.WRAP_CONTENT,
+                    android.widget.TableRow.LayoutParams.WRAP_CONTENT, 1.0f));
+            text.setText(marks.get(i));
+            tableRows[i].addView(text);
+
+            text = new TextView(this);
+            text.setLayoutParams(new android.widget.TableRow.LayoutParams(android.widget.TableRow.LayoutParams.WRAP_CONTENT,
+                    android.widget.TableRow.LayoutParams.WRAP_CONTENT, 1.0f));
+            text.setText(numOfQuestions.get(i));
+            tableRows[i].addView(text);
+
+            tableLayout.addView(tableRows[i]);
+
+
+//        for (int i=0; i<4;i++) {
+//            LinearLayout linearLayout = new LinearLayout(this);
+//            TextView ProgrammaticallyTextView = new TextView(this);
+//            ProgrammaticallyTextView.setText(" TextView Programmatically Example." + i);
+//            ProgrammaticallyTextView.setTextSize(22);
+//            ProgrammaticallyTextView.setPadding(20, 300, 20, 100);
+//
+//            linearLayout.addView(ProgrammaticallyTextView);
+//
+//            this.setContentView(linearLayout, new LinearLayout.LayoutParams(
+//                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+//
+//        }
+
+
+            ConnectivityManager connMgr = (ConnectivityManager)
+                    getSystemService(Context.CONNECTIVITY_SERVICE);
+            assert connMgr != null;
+            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isConnected()) {
+                new TMDBSearchManager().execute();
+            } else {
+                TextView textView = new TextView(this);
+                textView.setText("No network connection.");
+                setContentView(textView);
+                Log.e(Const.ERR, "stepErr");
+            }
+
         }
-
     }
 
-    private static class TMDBSearchManager extends AsyncTask {
+        class TMDBSearchManager extends AsyncTask {
 
-        @Override
-        protected ArrayList<SearchBuilder> doInBackground(Object... params) {
-            try {
-                return search();
-            } catch (IOException e) {
-                return null;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(Object result) {
-            Log.e(Const.DEBUG, "we're on the onPostExecute");
-        }
-
-        ArrayList<SearchBuilder> search() throws IOException {
-            // Build URL
-            String stringBuilder = "http://api.themoviedb.org/3/search/movie" +
-                    "?api_key=" + API.KEY + "&query=day%20after%20tomorrow";
-            URL url = new URL(stringBuilder);
-            Log.e(Const.SEE, url.toString());
-            Log.e(Const.TAG,url.toString());
-
-            InputStream stream = null;
-            try {
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(10000);
-                conn.setConnectTimeout(15000);
-                conn.setRequestMethod("GET");
-                conn.addRequestProperty("Accept", "application/json");
-                conn.setDoInput(true);
-                conn.connect();
-                int responseCode = conn.getResponseCode();
-                Log.e(Const.DEBUG, "The response code is: " + responseCode + " " + conn.getResponseMessage());
-                stream = conn.getInputStream();
-                return parseResult(stringify(stream));
-            } finally {
-                if (stream != null) {
-                    stream.close();
+            @Override
+            protected ArrayList<SearchBuilder> doInBackground(Object... params) {
+                try {
+                    return search();
+                } catch (IOException e) {
+                    return null;
                 }
             }
-        }
 
-        private ArrayList<SearchBuilder> parseResult(String result) {
-            ArrayList<SearchBuilder> results = new ArrayList<>();
+            @Override
+            protected void onPostExecute(Object result) {
+                Log.e(Const.DEBUG, "we're on the onPostExecute");
+            }
 
-            try {
-                JSONObject jsonObject = new JSONObject(result);
-                JSONArray array = (JSONArray) jsonObject.get("results");
-                for (int i = 0; i < array.length(); i++) {
-                    JSONObject jsonMovieObject = array.getJSONObject(i);
+            ArrayList<SearchBuilder> search() throws IOException {
+                // Build URL
+                String stringBuilder = "http://api.themoviedb.org/3/search/movie" +
+                        "?api_key=" + API.KEY + "&query=day%20after%20tomorrow";
+                URL url = new URL(stringBuilder);
+                Log.e(Const.SEE, url.toString());
+                Log.e(Const.TAG,url.toString());
 
-                    JSONArray gjIds = jsonMovieObject.getJSONArray("genre_ids");
-                    ArrayList<Integer> gIds = new ArrayList<>();
-                    for (int j=0; j<gjIds.length(); j++) {
-                        gIds.add( gjIds.getInt(j) );
+                InputStream stream = null;
+                try {
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setReadTimeout(10000);
+                    conn.setConnectTimeout(15000);
+                    conn.setRequestMethod("GET");
+                    conn.addRequestProperty("Accept", "application/json");
+                    conn.setDoInput(true);
+                    conn.connect();
+                    int responseCode = conn.getResponseCode();
+                    Log.e(Const.DEBUG, "The response code is: " + responseCode + " " + conn.getResponseMessage());
+                    stream = conn.getInputStream();
+                    return parseResult(stringify(stream));
+                } finally {
+                    if (stream != null) {
+                        stream.close();
                     }
-
-                    SearchBuilder.Builder searchBuilder = SearchBuilder.newBuilder(
-                            Integer.parseInt(jsonMovieObject.getString("id")),
-                            jsonMovieObject.getString("title"))
-                            .setOriginalTitle(jsonMovieObject.getString("original_title"))
-                            .setPosterPath(jsonMovieObject.getString("poster_path"))
-                            .setReleaseDate(jsonMovieObject.getString("release_date"))
-                            .setVoteAverage(Float.parseFloat(jsonMovieObject.getString("vote_average")))
-                            .setGenreIds(gIds);
-                    results.add(searchBuilder.build());
                 }
-            } catch (JSONException e) {
-                System.err.println(e);
-                Log.d(Const.DEBUG, "Error parsing JSON. String was: " + result);
             }
 
-            Log.e(Const.SEE, results.get(1).getOriginalTitle());
-            Log.e(Const.SEE, results.get(3).getGenreIds().toString());
-            return results;
+            private ArrayList<SearchBuilder> parseResult(String result) {
+                ArrayList<SearchBuilder> results = new ArrayList<>();
+
+                try {
+                    JSONObject jsonObject = new JSONObject(result);
+                    JSONArray array = (JSONArray) jsonObject.get("results");
+                    for (int i = 0; i < array.length(); i++) {
+                        JSONObject jsonMovieObject = array.getJSONObject(i);
+
+                        JSONArray gjIds = jsonMovieObject.getJSONArray("genre_ids");
+                        ArrayList<Integer> gIds = new ArrayList<>();
+                        for (int j=0; j<gjIds.length(); j++) {
+                            gIds.add( gjIds.getInt(j) );
+                        }
+
+                        SearchBuilder.Builder searchBuilder = SearchBuilder.newBuilder(
+                                Integer.parseInt(jsonMovieObject.getString("id")),
+                                jsonMovieObject.getString("title"))
+                                .setOriginalTitle(jsonMovieObject.getString("original_title"))
+                                .setPosterPath(jsonMovieObject.getString("poster_path"))
+                                .setReleaseDate(jsonMovieObject.getString("release_date"))
+                                .setVoteAverage(Float.parseFloat(jsonMovieObject.getString("vote_average")))
+                                .setGenreIds(gIds);
+                        results.add(searchBuilder.build());
+                    }
+                } catch (JSONException e) {
+                    System.err.println(e);
+                    Log.d(Const.DEBUG, "Error parsing JSON. String was: " + result);
+                }
+
+                Log.e(Const.SEE, results.get(1).getOriginalTitle());
+                Log.e(Const.SEE, results.get(3).getGenreIds().toString());
+                return results;
+            }
+
+
+            String stringify(InputStream stream) throws IOException {
+                Reader reader = new InputStreamReader(stream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(reader);
+                return bufferedReader.readLine();
+            }
+
         }
-
-
-        String stringify(InputStream stream) throws IOException {
-            Reader reader = new InputStreamReader(stream, "UTF-8");
-            BufferedReader bufferedReader = new BufferedReader(reader);
-            return bufferedReader.readLine();
-        }
-
     }
-}
