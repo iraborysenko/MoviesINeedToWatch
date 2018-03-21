5ef967dc22280a779d08a8222169c9ba9ffaf0c9
iraborysenko <aurorasray@gmail.com>
2018-03-21 22:30:03 +0200

    add removal from database

--- app/src/main/java/com/example/aurora/moviesineedtowatch/ui/MainActivity.java
+++ app/src/main/java/com/example/aurora/moviesineedtowatch/ui/MainActivity.java
@@ -3,17 +3,12 @@ package com.example.aurora.moviesineedtowatch.ui;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Typeface;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewConfiguration;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -25,7 +20,6 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
 import com.example.aurora.moviesineedtowatch.R;
 import com.example.aurora.moviesineedtowatch.tmdb.Const;
-import com.example.aurora.moviesineedtowatch.tmdb.DB;
 import com.example.aurora.moviesineedtowatch.tmdb.MovieBuilder;
 
 import org.json.JSONArray;
@@ -37,17 +31,8 @@ import io.realm.Realm;
 import io.realm.RealmResults;
 
 import static com.example.aurora.moviesineedtowatch.tmdb.Const.DEBUG;
-import static com.example.aurora.moviesineedtowatch.tmdb.Const.GENRES_IDS;
-import static com.example.aurora.moviesineedtowatch.tmdb.Const.ID;
-import static com.example.aurora.moviesineedtowatch.tmdb.Const.ID_MOVIE;
-import static com.example.aurora.moviesineedtowatch.tmdb.Const.IMDB;
-import static com.example.aurora.moviesineedtowatch.tmdb.Const.LANG;
-import static com.example.aurora.moviesineedtowatch.tmdb.Const.POST_IMAGE;
-import static com.example.aurora.moviesineedtowatch.tmdb.Const.RELEASE_DATE;
 import static com.example.aurora.moviesineedtowatch.tmdb.Const.SEE;
 import static com.example.aurora.moviesineedtowatch.tmdb.Const.SHARED_REFERENCES;
-import static com.example.aurora.moviesineedtowatch.tmdb.Const.TAGLINE;
-import static com.example.aurora.moviesineedtowatch.tmdb.Const.TITLE;
 import static com.example.aurora.moviesineedtowatch.tmdb.Const.genres;
 import static com.example.aurora.moviesineedtowatch.ui.MovieActivity.stringToBitmap;
 
@@ -75,15 +60,17 @@ public class MainActivity extends AppCompatActivity {
         Realm.init(this);
         mRealm = Realm.getDefaultInstance();
 
-//        showDBData();
         displayListOfMovies();
 
     }
 
     @SuppressLint("ResourceType")
     private void displayListOfMovies() {
+        mTable.removeAllViewsInLayout();
+
         mRealm.beginTransaction();
         RealmResults<MovieBuilder> movies = mRealm.where(MovieBuilder.class).findAll();
+        mRealm.commitTransaction();
         MovieBuilder movie;
 
         if(!movies.isEmpty()) {
@@ -211,17 +198,22 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-//                tr.setOnLongClickListener(new View.OnLongClickListener() {
-//                    @Override
-//                    public boolean onLongClick(View v) {
-//                        Log.e(Const.TAG, "it's long click listener");
-//                        DB db1 = new DB(MainActivity.this);
-//                        SQLiteDatabase db = db1.getWritableDatabase();
-//                        db.delete(DB.TABLE_MOVIE, "id = " + rowId, null);
-//                        showDBData();
-//                        return true;
-//                    }
-//                });
+                tr.setOnLongClickListener(new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View v) {
+                        mRealm.beginTransaction();
+
+                        RealmResults<MovieBuilder> result = mRealm.where(MovieBuilder.class)
+                                .equalTo("id", movieId)
+                                .equalTo("savedLang", dataLang)
+                                .findAll();
+                        result.deleteAllFromRealm();
+                        mRealm.commitTransaction();
+
+                        displayListOfMovies();
+                        return true;
+                    }
+                });
 
 
                 mTable.addView(tr);
@@ -230,7 +222,6 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Log.e(DEBUG, "No data yet.");
         }
-        mRealm.commitTransaction();
 
     }
 
@@ -242,7 +233,7 @@ public class MainActivity extends AppCompatActivity {
         boolean is_changed = settings.getBoolean("db_is_changed", false);
         if(is_changed){
             Log.e(Const.TAG, "I'm onRestart!");
-//            showDBData();
+            displayListOfMovies();
             SharedPreferences.Editor editor = getSharedPreferences(SHARED_REFERENCES, MODE_PRIVATE).edit();
             editor.putBoolean("db_is_changed", false);
             editor.apply();
@@ -258,155 +249,6 @@ public class MainActivity extends AppCompatActivity {
     public static Realm getRealm() {
         return mRealm;
     }
-//
-//    @SuppressLint("ResourceType")
-//    private void showDBData() {
-//        mTable.removeAllViewsInLayout();
-//
-//        DB db = new DB(MainActivity.this);
-//        Cursor cursor = db.getAllMovies();
-//
-//        if (cursor.moveToFirst()) {
-//            while (!cursor.isAfterLast()) {
-//
-//                final String rowId = cursor.getString(ID);
-//
-//                RelativeLayout tr = new RelativeLayout(MainActivity.this);
-//
-//                //get poster
-//                ImageView mPoster = new ImageView(MainActivity.this);
-//                mPoster.setId(1);
-//
-//                RequestOptions options = new RequestOptions()
-//                        .skipMemoryCache(true)
-//                        .diskCacheStrategy(DiskCacheStrategy.NONE);
-//                Glide.with(this)
-//                        .asBitmap()
-//                        .load(stringToBitmap(cursor.getString(POST_IMAGE)))
-//                        .apply(options)
-//                        .into(mPoster);
-//
-//                //title
-//                final TextView mTitle = new TextView(MainActivity.this);
-//                mTitle.setId(2);
-//                mTitle.setText(cursor.getString(TITLE));
-//                mTitle.setTypeface(font, Typeface.BOLD);
-//                mTitle.setGravity(Gravity.CENTER);
-//                mTitle.setTextColor(getResources().getColor(R.color.colorDarkGreen));
-//                mTitle.setTextSize(20);
-//
-//                //tagline
-//                TextView mTagline = new TextView(MainActivity.this);
-//                mTagline.setId(3);
-//                mTagline.setText(cursor.getString(TAGLINE));
-//                mTagline.setTypeface(font, Typeface.BOLD_ITALIC);
-//                mTagline.setTextSize(16);
-//
-//                //get genres
-//                StringBuilder genresString = new StringBuilder();
-//                try {
-//                    JSONArray ids = new JSONArray(cursor.getString(GENRES_IDS));
-//                    if (ids.length() == 0) {
-//                        genresString = new StringBuilder("not defined");
-//                    } else {
-//                        int index = (Objects.equals(cursor.getString(LANG), "true"))?0:1;
-//                        for (int i=0; i<ids.length(); i++)
-//                            genresString.append(genres.get(ids.get(i))[index]).append("\n");
-//                    }
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
-//
-//                TextView mGenres = new TextView(MainActivity.this);
-//                mGenres.setId(4);
-//                mGenres.setText(String.valueOf(genresString.toString()));
-//                mGenres.setTypeface(font, Typeface.BOLD);
-//                mGenres.setTextColor(getResources().getColor(R.color.colorLightBlue));
-//
-//                //get year
-//                TextView mYear = new TextView(MainActivity.this);
-//                mYear.setId(5);
-//                mYear.setText(cursor.getString(RELEASE_DATE));
-//                mYear.setTypeface(font, Typeface.BOLD);
-//                mYear.setGravity(Gravity.CENTER);
-//
-//                //get IMDb rating
-//                TextView mIMDb = new TextView(MainActivity.this);
-//                mIMDb.setId(6);
-//                mIMDb.setText(cursor.getString(IMDB));
-//                mIMDb.setTypeface(font, Typeface.BOLD);
-//                mIMDb.setBackgroundColor(getResources().getColor(chooseColor(cursor.getString(IMDB))));
-//                mIMDb.setTextColor(getResources().getColor(R.color.colorBeige));
-//                mIMDb.setTextSize(18);
-//                mIMDb.setGravity(Gravity.CENTER);
-//
-//
-//                RelativeLayout.LayoutParams posterParams = new RelativeLayout.LayoutParams(
-//                        230, 490);
-//                posterParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
-//                posterParams.setMargins(20,0,15, 20);
-//                tr.addView(mPoster, posterParams);
-//
-//                RelativeLayout.LayoutParams titleParams = new RelativeLayout.LayoutParams(
-//                        RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-//                titleParams.addRule(RelativeLayout.ALIGN_TOP);
-//                tr.addView(mTitle, titleParams);
-//
-//                RelativeLayout.LayoutParams taglineParams = new RelativeLayout.LayoutParams(
-//                        550, RelativeLayout.LayoutParams.WRAP_CONTENT);
-//                taglineParams.addRule(RelativeLayout.RIGHT_OF, mPoster.getId());
-//                taglineParams.addRule(RelativeLayout.BELOW, mTitle.getId());
-//                taglineParams.setMargins(0, 7, 20, 10);
-//                tr.addView(mTagline, taglineParams);
-//
-//                RelativeLayout.LayoutParams genresParams = new RelativeLayout.LayoutParams(
-//                        235, RelativeLayout.LayoutParams.WRAP_CONTENT);
-//                genresParams.addRule(RelativeLayout.RIGHT_OF, mPoster.getId());
-//                genresParams.addRule(RelativeLayout.BELOW, mTagline.getId());
-//                genresParams.setMargins(0, 0, 5, 10);
-//                tr.addView(mGenres, genresParams);
-//
-//                RelativeLayout.LayoutParams imdbParams = new RelativeLayout.LayoutParams(
-//                        190, 230);
-//                imdbParams.addRule(RelativeLayout.BELOW, mTagline.getId());
-//                imdbParams.addRule(RelativeLayout.RIGHT_OF, mGenres.getId());
-//                imdbParams.setMargins(10, 5, 20, 10);
-//                tr.addView(mIMDb, imdbParams);
-//
-//                RelativeLayout.LayoutParams yearParams = new RelativeLayout.LayoutParams(
-//                        200, RelativeLayout.LayoutParams.WRAP_CONTENT);
-//                yearParams.addRule(RelativeLayout.BELOW, mIMDb.getId());
-//                yearParams.addRule(RelativeLayout.RIGHT_OF, mGenres.getId());
-//                yearParams.setMargins(10, 0, 20, 20);
-//                tr.addView(mYear, yearParams);
-//
-//                tr.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v)
-//                    {
-//                        movieTMDB(rowId);
-//                    }
-//                });
-//
-//                tr.setOnLongClickListener(new View.OnLongClickListener() {
-//                    @Override
-//                    public boolean onLongClick(View v) {
-//                        Log.e(Const.TAG, "it's long click listener");
-//                        DB db1 = new DB(MainActivity.this);
-//                        SQLiteDatabase db = db1.getWritableDatabase();
-//                        db.delete(DB.TABLE_MOVIE, "id = " + rowId, null);
-//                        showDBData();
-//                        return true;
-//                    }
-//                });
-//
-//
-//                mTable.addView(tr);
-//                cursor.moveToNext();
-//            }
-//        }
-//        cursor.close();
-//    }
 
     private int chooseColor(String imdbRating) {
         int color = R.color.OutOfBound;
