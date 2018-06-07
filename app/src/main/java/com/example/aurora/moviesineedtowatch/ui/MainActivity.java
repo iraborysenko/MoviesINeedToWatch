03ba724da5bd79f9c5cda4574b6f985a1c591ae1
iraborysenko <aurorasray@gmail.com>
2018-06-07 20:08:04 +0300

    add update movies list

--- app/src/main/java/com/example/aurora/moviesineedtowatch/ui/MainActivity.java
+++ app/src/main/java/com/example/aurora/moviesineedtowatch/ui/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.aurora.moviesineedtowatch.ui;
 
-import static com.example.aurora.moviesineedtowatch.tmdb.Const.SHARED_REFERENCES;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -21,6 +19,8 @@ import com.example.aurora.moviesineedtowatch.tmdb.Movie;
 
 import java.util.Objects;
 
+import io.realm.OrderedCollectionChangeSet;
+import io.realm.OrderedRealmCollectionChangeListener;
 import io.realm.Realm;
 import io.realm.RealmResults;
 
@@ -45,23 +45,16 @@ public class MainActivity extends AppCompatActivity {
         });
 
         initRealm();
-        RealmResults<Movie> movies = getMoviesFromDB();
-        initRecyclerView(movies);
-    }
+        final RealmResults<Movie> movies = getMoviesFromDB();
+        final RecyclerAdapter mAdapter = initRecyclerView(movies);
 
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        //TODO update data
-//        SharedPreferences settings = getSharedPreferences(SHARED_REFERENCES, MODE_PRIVATE);
-//        boolean is_changed = settings.getBoolean("db_is_changed", false);
-//        if(is_changed){
-//            Log.i(Const.TAG, "I'm onRestart!");
-////            displayListOfMovies();
-//            SharedPreferences.Editor editor = getSharedPreferences(SHARED_REFERENCES, MODE_PRIVATE).edit();
-//            editor.putBoolean("db_is_changed", false);
-//            editor.apply();
-//        }
+        movies.addChangeListener(new OrderedRealmCollectionChangeListener<RealmResults<Movie>>() {
+            @Override
+            public void onChange(@NonNull RealmResults<Movie> results,
+                                 @NonNull OrderedCollectionChangeSet changeSet) {
+                mAdapter.notifyDataSetChanged();
+            }
+        });
     }
 
     @Override
@@ -86,17 +79,16 @@ public class MainActivity extends AppCompatActivity {
         return movies;
     }
 
-    private void initRecyclerView(RealmResults<Movie> movies) {
-        RecyclerView mRecyclerView = findViewById(R.id.movie_recycler_view);
+    private RecyclerAdapter initRecyclerView(RealmResults<Movie> movies) {
+        final RecyclerView mRecyclerView = findViewById(R.id.movie_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        RecyclerAdapter mAdapter = new RecyclerAdapter(movies, getApplicationContext(), getResources());
+        final RecyclerAdapter mAdapter = new RecyclerAdapter(movies, getApplicationContext(), getResources());
         mRecyclerView.setAdapter(mAdapter);
 
         mAdapter.setOnItemClickListener(new RecyclerAdapter.ClickListener() {
             @Override
             public void onItemClick(View v, String movieId, String dataLang) {
                 movieTMDB(movieId, dataLang);
-                Log.e(Const.TAG, "onItemClick position: " + movieId);
             }
 
             @Override
@@ -108,9 +100,9 @@ public class MainActivity extends AppCompatActivity {
                         .findAll();
                 result.deleteAllFromRealm();
                 sRealm.commitTransaction();
-                Log.e(Const.TAG, "onItemLongClick pos = " + dataLang);
             }
         });
+        return mAdapter;
     }
 
     private void movieTMDB(String movieId, String dataLang) {
